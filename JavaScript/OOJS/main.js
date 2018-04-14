78d3e66e5ea971709d9657d4f83ec051ef2ff50f
lalit527 <lalit.slg007@gmail.com>
2018-04-14 08:10:44 +0530

    this in javascript

---
+++ JavaScript/OOJS/main.js
@@ -0,0 +1,15 @@
+//this
+
+1) In the global scope, 'this' will refer to
+    the window object.
+
+2) Inside a object, 'this' will refer to the
+    current object.
+
+3) If i call a function with 'call', 'apply', 'bind'
+    'this' will refer to the object with which
+    the function is called.
+
+4) If i create an object with 'new' keyword,
+    this will refer to the newly created object
+  
