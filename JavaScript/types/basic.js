e8b28e199f0af1556ae63193a7404cdc7ff84c83
lalit527 <lalit.slg007@gmail.com>
2018-04-15 08:10:39 +0530

    typeof basics

---
+++ JavaScript/types/basic.js
@@ -0,0 +1,11 @@
+typeof a      // "undefined"
+typeof "a"    // "string"
+typeof 123    // "number"
+typeof true   // "boolean"
+typeof { a: 1 } // "object"
+typeof function() { }  // "function"
+typeof [1, 2, 3] // "object"
+
+typeof null  // "object"
+
+typeof undefined // "undefined"
