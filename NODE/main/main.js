0a278ec24324e413e88862728c3962d9fba722aa
lalit527 <lalit.slg007@gmail.com>
2018-05-12 06:51:01 +0530

    Before class of Angular

--- NODE/main/main.js
+++ NODE/main/main.js
@@ -1,2 +1,4 @@
 const moment = require('moment');
-console.log(moment("20180508T16:30:00.958+05:00", "YYYYMMDD HH:MM:SS").fromNow());
+console.log(moment("2018-05-09T11:49:00+05:30").fromNow());
+
+console.log(moment().toDate());
