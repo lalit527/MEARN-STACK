f48dd62b5b9d22745387079fc08ba887dc1e7e34
lalit527 <lalit.slg007@gmail.com>
2018-07-28 13:58:27 +0530

    Making post call from client

--- NODE/Batch2/Day-5-http-server/client-server/client.js
+++ NODE/Batch2/Day-5-http-server/client-server/client.js
@@ -1,10 +1,11 @@
-var http = require('http');
+const http = require('http');
+const parse = require('querystring');
 
 const { URL } = require('url');
 
 const option = new URL("http://localhost:8005");
 
-var req = http.get(option, (res) => {
+http.get(option, (res) => {
   let data = '';
   res.on('data', (chunk) => {
     data += chunk;
@@ -17,4 +18,37 @@ var req = http.get(option, (res) => {
   console.log("Error: " + err.message);
 });
 
-console.log('Data');
+const postData = parse.stringify({
+  'msg': 'Hello World!'
+});
+
+const options = {
+  hostname: 'localhost',
+  port: 8005,
+  path: '/',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': Buffer.byteLength(postData)
+  }
+};
+
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+// write data to request body
+req.write(postData);
+req.end();
