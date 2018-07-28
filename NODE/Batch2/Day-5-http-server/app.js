df4f80a88108bcf206f13601a5a8df0fb5fb4c3f
lalit527 <lalit.slg007@gmail.com>
2018-07-28 13:13:39 +0530

    Added post for http node

--- NODE/Batch2/Day-5-http-server/app.js
+++ NODE/Batch2/Day-5-http-server/app.js
@@ -1,20 +1,25 @@
 const http = require('http');
+const parse = require('querystring');
 
 const server = http.createServer((req, res) => {
-  // console.log(req.method);
-  // res.write('Hello World');
-  // res.write('Hello from Node JS');
-  // res.write('Node is awesome');
-  // res.write('Nodemon is awesome');
-  // res.write('I have created a server');
-  // res.end();
-  if (req.method == "GET") {
+  if(req.method === 'POST') {
+    let body = [];
+    req.on('data', (chunk) => {
+      console.log('ch',chunk.toString());
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      res.write(`{data: ${body}}`);
+      console.log(body);
+      res.end();
+      // at this point, `body` has the entire request body stored in it as a string
+    });
+  } else if (req.method == "GET") {
     console.log(req.url)
     res.write('Hello World GET');
     res.end();
   }
 });
-
 
 server.on('clientError', (err, socket) => {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
