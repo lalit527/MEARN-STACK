6787da7219a1d5df168641d75eca4ce4e43d7efb
lalit527 <lalit.slg007@gmail.com>
2018-11-25 17:13:50 +0530

    Added File Handling in Node

--- NODE/Batch4/Day2/project1/app.js
+++ NODE/Batch4/Day2/project1/app.js
@@ -1,21 +1,43 @@
 const http = require('http');
+const fs = require('fs');
+const { parse } = require('querystring');
+
+const fileHelper = require('./fileHelper');
+
+
 let handler = (req, res) => {
   console.log(req.url);
-  // console.log(req.method);
-  // console.log(req.headers);
   if (req.url === '/') {
     res.write('<h1>Hello World</h1>');
+    let data = fileHelper.readFileData('todo.txt');
+    let result = fileHelper.writeFileData('todo1.txt', data);
+    
   } else if (req.url === '/node') {
     res.write('<h1>Hello Node</h1>');
   } else if (req.url === '/python') {
     res.setHeader('x-auth-token', 'khebwfhbelwbbjb');
     res.write('<h1>Hello Node</h1>');
     res.write(`<form method="post" action="/save_user">
-                <input type="text" name="name" placeholder="name">
+                <input type="text" name="task" placeholder="task">
+                <input type="text" name="staus" placeholder="status">
                 <input type="submit" value="Submit">
               </form>`);
   } else if (req.method === 'POST' && req.url === '/save_user') {
-    console.log(req);
+    let result = [];
+
+    req.on('data', data => {
+      result.push(data);
+    });
+
+    req.on('end', data => {
+      console.log('result', result);
+      console.log(parse(result.join(' ').toString()));
+      console.log('data', data);
+    });
+
+    res.writeHead(301, {Location: '/python'});
+  } else {
+    res.writeHead(301, {Location: '/'});
   }
   res.end();
 }
---
+++ NODE/Batch4/Day2/project1/fileHelper.js
@@ -0,0 +1,23 @@
+let readFileData = (filename) => {
+  fs.readFile(filename, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result.toString());
+    }
+  });
+};
+let writeFileData = (filename, data) => {
+  fs.writeFile(filename, data, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
+};
+
+module.exports = {
+  readFileData,
+  writeFileData
+}
