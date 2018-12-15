5e2afa30b85c010ab13e2e290b169a3f8e622d11
lalit527 <lalit.slg007@gmail.com>
2018-12-15 15:13:06 +0530

    Solution file for Day2

---
+++ NODE/Batch4/Day2/project1-sol/app.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const { parse } = require('querystring');
+
+const fileHelper = require('./fileHelper');
+
+
+let handler = (req, res) => {
+  console.log(req.url);
+  if (req.url === '/') {
+    let data = fileHelper.readFileData('todo.txt');
+    let result = fileHelper.writeFileData('todo1.txt', data);
+    res.write(`<h1>Hello World</h1>${data}`);
+    
+  } else if (req.url === '/node') {
+    res.write('<h1>Hello Node</h1>');
+  } else if (req.url === '/python') {
+    res.setHeader('x-auth-token', 'khebwfhbelwbbjb');
+    res.write('<h1>Hello Node</h1>');
+    res.write(`<form method="post" action="/save_user">
+                <input type="text" name="task" placeholder="task">
+                <input type="text" name="staus" placeholder="status">
+                <input type="submit" value="Submit">
+              </form>`);
+  } else if (req.method === 'POST' && req.url === '/save_user') {
+    let result = [];
+
+    req.on('data', data => {
+      result.push(data);
+    });
+
+    req.on('end', data => {
+      console.log('result', result);
+      console.log(parse(result.join(' ').toString()));
+      console.log('data', data);
+    });
+
+    res.writeHead(301, {Location: '/python'});
+  } else {
+    res.writeHead(301, {Location: '/'});
+  }
+  res.end();
+}
+const server = http.createServer(handler);
+
+server.listen(7002, () => {
+  console.log(`Server started on port 7002`);
+});
