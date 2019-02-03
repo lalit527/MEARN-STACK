64894672f249d83e1cc09121df0673afc662ce75
lalit527 <lalit.slg007@gmail.com>
2019-02-03 15:12:02 +0530

    Added Session cookie

--- NODE/Batch4/Day12/app.js
+++ NODE/Batch4/Day12/app.js
@@ -6,12 +6,25 @@ const router = require('./app/routes');
 const mongoConnect = require('./utils/database');
 const user = require('./app/user/user.model');
 const path = require('path');
+const cookie = require('cookie-parser');
+const session = require('express-session');
 // const userRoutes = require('./app/user/user.router')();
 
 
 // app.use('/user/v1', router);
 app.use(bodyParser.json({limit:'10mb',extended:true}));
 app.use(bodyParser.urlencoded({limit:'10mb',extended:true}));
+
+app.use(cookie());
+
+app.use(session({
+  name: 'batch4Cookie',
+  secret: 'batch4Secret',
+  resave: true,
+  httpOnly: true,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}));
 
 app.set('view engine', 'pug');
 app.set('views',path.join(__dirname + '/app/views'));
--- NODE/Batch4/Day12/app/user/user.model.js
+++ NODE/Batch4/Day12/app/user/user.model.js
@@ -18,12 +18,15 @@ const userSchema = new Schema({
 userSchema.statics.findByCredential = function(email, password) {
   const user = this;
   return new Promise((resolve, reject) => {
+    console.log('cred', email, password);
     user.findOne({email}).then((result) => {
+      console.log('success', result);
       if (password === result.password) {
         return resolve(result);
       }
       return reject(`Passord doesnt match for ${result.email}`);
     }).catch((err) => {
+      console.log('fails', email);
       return reject('Email Not Found');
     })
   })
