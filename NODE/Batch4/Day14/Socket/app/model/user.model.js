acc5042715a95f5da9c4ce39445f8b70f846e46c
lalit527 <lalit.slg007@gmail.com>
2019-02-23 16:57:44 +0530

    Added socket programs private room

--- NODE/Batch4/Day14/Socket/app/model/user.model.js
+++ NODE/Batch4/Day14/Socket/app/model/user.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 let user = new Schema({
 	name 			: {type:String, required:true},
-	usename   : {type:String, required:true},
+	usename   : {type:String, required:true, unique: true},
 	mobile  	: {type:String, required:true},
 	email     : {type:String, required:true},
 	password  : {type:String, required:true}
